4069c7fd0fb244918b3482dae1b298f61e5e4e72
chinchu141 <76766513+chinchu141@users.noreply.github.com>
2021-05-23 12:35:31 +0530

    Update AccountVO.java

--- src/main/java/com/scg/employeemanagement/vo/AccountVO.java
+++ src/main/java/com/scg/employeemanagement/vo/AccountVO.java
@@ -1,20 +1,14 @@
 package com.scg.employeemanagement.vo;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
 
 @Data
-@Getter
-@Setter
-@ToString
+
 public class AccountVO {
 
 	private String accountNumber;
 	private String branch;
 	private String ifsc;
 
-	// private Employee employee;
 
 }
