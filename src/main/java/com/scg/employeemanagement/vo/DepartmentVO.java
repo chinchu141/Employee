4980bb1d9c41a7f9506f90bbad3ca0b3a34d1478
chinchu141 <76766513+chinchu141@users.noreply.github.com>
2021-05-23 12:45:40 +0530

    Update DepartmentVO.java

--- src/main/java/com/scg/employeemanagement/vo/DepartmentVO.java
+++ src/main/java/com/scg/employeemanagement/vo/DepartmentVO.java
@@ -5,20 +5,16 @@ import java.util.List;
 import com.scg.employeemanagement.dao.entity.Employee;
 
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+
 
 
 @Data
-@Getter
-@Setter
-@ToString
+
 public class DepartmentVO {
 
 	private int departmentId;
 	private String departmentName;
-	private List<Employee> employees;
+	private List<EmployeeVO> employees;
 	private AuditVO audit;
 	
 	
