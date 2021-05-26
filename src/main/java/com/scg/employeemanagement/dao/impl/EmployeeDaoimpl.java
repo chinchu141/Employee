6fad5168e391d76909d6cba173b0f7de7e282235
chinchu141 <76766513+chinchu141@users.noreply.github.com>
2021-05-26 14:02:40 +0530

    add Query annotation

--- src/main/java/com/scg/employeemanagement/dao/impl/EmployeeDaoimpl.java
+++ src/main/java/com/scg/employeemanagement/dao/impl/EmployeeDaoimpl.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+
 import com.scg.employeemanagement.LogExecutionTime;
 import com.scg.employeemanagement.dao.EmployeeDao;
 import com.scg.employeemanagement.dao.entity.Employee;
@@ -13,6 +14,7 @@ import com.scg.employeemanagement.exception.ApiException;
 import com.scg.employeemanagement.exception.ErrorCode;
 import com.scg.employeemanagement.exception.Validator;
 import com.scg.employeemanagement.mapper.EmployeeMapper;
+
 import com.scg.employeemanagement.vo.EmployeeVO;
 
 @Component
@@ -28,44 +30,77 @@ public class EmployeeDaoimpl implements EmployeeDao {
 	@Override
 	public EmployeeVO addEmployee(EmployeeVO employeeVO) {
 		Validator.validate(employeeVO);
-		Employee employee = employeeMapper.toEntity(employeeVO);
-		return employeeMapper.toVO(employeeRepository.save(employee));
+		
+		return employeeMapper.toVO(employeeRepository.save(employeeMapper.toEntity(employeeVO)));
 
 	}
 
-	@LogExecutionTime
+	
 	@Override
 	public List<EmployeeVO> getEmployees() {
+		
 		return employeeMapper.toVO(employeeRepository.findAll());
 
 	}
+	
+//	@Override
+//	public List<EmployeeVO> getEmployeeByName(String name) {
+//		
+//		return employeeMapper.toVO(employeeRepository.findByemployeeNameContaining();
+//
+//
+//	}
 
+    @LogExecutionTime
 	@Override
 	public String deleteEmployee(int id) {
-		
+	
 		getEmployeeById(id);
+		
+		
 		employeeRepository.deleteById(id);
 		return "Employee id : " + id + " Removed";
 
 	}
-
+ 
 	@Override
 	public EmployeeVO updateEmployee(EmployeeVO employeeVO) {
 		Validator.validate(employeeVO);
 		Employee employee = employeeMapper.toEntity(employeeVO);
-		Employee existingEmployee = employeeMapper.toEntity(getEmployeeById(employee.getEmployeeId()));
+		Employee existingEmployee =employeeRepository.findById(employee.getEmployeeId()).orElseThrow(() -> new ApiException(ErrorCode.EMPLOYEE_NOT_FOUND));
 		existingEmployee.setEmployeeName(employee.getEmployeeName());
 		existingEmployee.setAccounts(employee.getAccounts());
 		return employeeMapper.toVO(employeeRepository.save(existingEmployee));
 
 	}
-
 	@Override
 	public EmployeeVO getEmployeeById(final int id) {
 		Validator.validateId(id);
-		EmployeeVO employeeVo = employeeMapper.toVO(employeeRepository.findById(id).orElseThrow(() -> new ApiException(ErrorCode.EMPLOYEE_NOT_FOUND)));
-		return employeeVo;
+		return  employeeMapper.toVO(employeeRepository.findById(id).orElseThrow(() -> new ApiException(ErrorCode.EMPLOYEE_NOT_FOUND)));
+		
+		
 
+	}
+
+
+	@Override
+	public List<String> getDepartmentNameByEmployeeId(int id) {
+		
+		return employeeRepository.findDepartmentByEmployeeId(id);
+	}
+
+
+	@Override
+	public List<String> getEmployeeAndDepartmentName() {
+		
+		return employeeRepository.findEmployeeAndDepartmentName();
+	}
+
+
+	@Override
+	public List<String> getAccountDetailsByEmployeeId(int id) {
+
+		return employeeRepository.findAccountDetailsByEmployeeId(id);
 	}
 
 }
